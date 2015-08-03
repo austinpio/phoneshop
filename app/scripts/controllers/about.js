3cb558f5beaccc2a46cfe4fd4abc77b99ac8fb64
austin <austin@visenti.com>
2015-08-03 13:08:59 +0800

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name phoneshopApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the phoneshopApp
+ */
+angular.module('phoneshopApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name phoneshopApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the phoneshopApp
+ */
+angular.module('phoneshopApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
